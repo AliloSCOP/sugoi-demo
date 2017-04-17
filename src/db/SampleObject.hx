17d130d2f7f7f6be98c0f1885cbae409ac0f88a4
fbarbut <francois.barbut@gmail.com>
2017-04-17 17:05:32 +0200

    load bootstrap from CDN

---
+++ src/db/SampleObject.hx
@@ -0,0 +1,25 @@
+package db;
+import sys.db.Object;
+import sys.db.Types;
+
+
+
+/**
+ * SPOD Object for testing forms
+ * @author fbarbut
+ */
+class SampleObject extends Object
+{
+
+	public var id:SId;
+	
+	public var name:SString<32>;
+	
+	//public var type : SEnum<UserType>;
+	
+	public var xp : SInt;
+	
+	
+	
+	
+}
