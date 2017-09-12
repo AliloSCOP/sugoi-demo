d35bebc4024f9cb595ed2fc001f87ca773114839
fbarbut <francois.barbut@gmail.com>
2017-09-12 12:15:31 +0200

    plugins

--- src/App.hx
+++ src/App.hx
@@ -1,6 +1,6 @@
 package ;
 import sugoi.i18n.Locale;
-
+import Common;
 import sugoi.i18n.GetText;
 import sugoi.Web;
 
@@ -9,9 +9,26 @@ class App extends sugoi.BaseApp
 	public static var current : App = null;
 	public static var config = sugoi.BaseApp.config;
 	
+	//plugin mgmt
+	public var eventDispatcher :hxevents.Dispatcher<Event>;	
+	public var plugins : Array<sugoi.plugin.IPlugIn>;
+	
 	public static function main() {
 		
 		sugoi.BaseApp.main();
+	}
+	
+	/**
+	 * Init plugins and event dispatcher just before launching the app
+	 */
+	override public function mainLoop() {
+		eventDispatcher = new hxevents.Dispatcher<Event>();
+		plugins = [];
+	
+		//init plugins
+		plugins.push(new demoplugin.DemoPlugin());
+	
+		super.mainLoop();
 	}
 	
 	public static function log(t:Dynamic) {
